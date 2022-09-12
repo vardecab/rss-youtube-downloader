c42ad729b8b22368678912076ba12f8321ed8129
vardecab <6877391+vardecab@users.noreply.github.com>
2022-09-12 16:14:50 +0200

    0.3

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# youtube-downloader
+# rss-youtube-downloader
 
 ![](https://img.shields.io/badge/platform-Windows%20%7C%20macOS-blue)
 
@@ -18,6 +18,7 @@
 
 ## Release History
 
+- 0.3: Fixed the script on macOS; added checker to avoid duplicates; re-wrote the script to handle multiple videos.
 - 0.2: Added RSS reader so now the script downloads the latest video from the RSS feed.
 - 0.1: Initial release.
 
--- run.py
+++ run.py
@@ -1,15 +1,22 @@
 # ==================================== #
-#          youtube-downloader          #
+#        rss-youtube-downloader        #
 # ==================================== #
 
 # ------------ import libs ----------- #
 
 import time # calculate script's run time
 from datetime import datetime # generate timestamp for saving data
-import os # create folders
+# import os # create folders
+import sys # check OS
 
-import feedparser # read RSS
 import yt_dlp # download YouTube videos # NOTE: https://github.com/ytdl-org/youtube-dl/issues/30102#issuecomment-943849906
+import feedparser # read RSS
+
+# NOTE: fix certificate issue -> https://stackoverflow.com/questions/28282797/feedparser-parse-ssl-certificate-verify-failed
+if sys.platform != 'win32': # check if user is using macOS
+    import ssl
+    if hasattr(ssl, '_create_unverified_context'):
+        ssl._create_default_https_context = ssl._create_unverified_context
 
 # --------- start + run time --------- #
 
@@ -18,7 +25,7 @@ print("Starting the script...") # status
 
 # ----- timestamp for saving data ---- #
 
-this_run_datetime = timestamp = datetime.strftime(datetime.now(), '%y%m%d-%H%M%S') # eg. 210120-173112
+# this_run_datetime = timestamp = datetime.strftime(datetime.now(), '%y%m%d-%H%M%S') # eg. 210120-173112
 
 # ----- check and create folders ----- #
 
@@ -29,50 +36,97 @@ this_run_datetime = timestamp = datetime.strftime(datetime.now(), '%y%m%d-%H%M%S
 
 # ---------- fun begins here --------- #
 
+# ---------- download video ---------- #
+
+# TODO: exclude segments with SponsorBlock parameters
+# TODO: convert to .mp3?
+
+def downloadVideo(videoURL):
+    
+    downloadPath = r'/Users/x/TV Shows/' # download location
+
+    #  parameters for the downloader
+    ydl_opts = { 
+        'quiet': True, # don't throw status messages in the console
+        'outtmpl': downloadPath + r'/%(title)s.%(ext)s' # download location + name of the file
+        } 
+
+    try:
+        with yt_dlp.YoutubeDL(ydl_opts) as ydl:
+            # info_dict = ydl.extract_info(videoURL, download=False) # get info (title, metadata, etc.) about the video without downloading
+            
+            print("Downloading the video...") # status
+            ydl.download(videoURL) # now download the video
+            print("Video downloaded or it's already on the disk.") # status
+            # TODO: how to check if downloading or already on disk?
+            
+            with open('videos.txt','a') as saveFile: # create and save file with downloaded videos' URLs
+                saveFile.write(videoURL + "\n")
+                # NOTE: not very pretty but works
+                    # TODO: avoid duplicates 
+
+    except: # Internet down, wrong URL
+        print(f"Can't download the video. Check your internet connection and video's URL ({videoURL}), then try again. Closing...") # status
+
 # ------------- read RSS ------------- #
+
+# TODO: get input + default on timeout 
+# TODO: ask which feed to check, type 1/2/3/4/5 or name of the channel 
+# TODO: check multiple feeds at once? function?
+# TODO: notifications when something is found / not found
 
 RSSfeed = 'https://www.youtube.com/feeds/videos.xml?channel_id=UC8LJZNHnqXKg5TMgyvxszPA' 
 print("Reading RSS feed...") # status
 
 try:
     read_RSS = feedparser.parse(RSSfeed) # read the RSS feed
-    # print(read_RSS.feed.link) # print channel URL 
-    videoTitle = read_RSS.entries[0].title # get latest video's title
-    print(f"Latest video's title: {videoTitle}") # print the latest video's URL
-    videoURL = read_RSS.entries[0].link # get the latest video's URL
-    print(f"Latest video's URL: {videoURL}") # print the latest video's URL
-# except (IndexError): # Internet down, wrong URL
-except: # Internet down, wrong URL
-    print("Can't read RSS file. Check your internet connection and RSS feed's URL, then try again. Closing...") # status
+except: 
+    print(f"Can't read the RSS feed. Maybe there is a problem with your internet connection or with the `RSSfeed` URL: {RSSfeed}. Closing...")
+    exit()
 
-# TODO: check which video was downloaded last and go from there
-# print(read_RSS.entries[1].link) # get previous video's URL
-
-# ---------- download video ---------- #
-
-# TODO: exclude segments with SponsorBlock parameters
-
-# downloadPath = '' # TODO: change
-
-#  parameters for the downloader
-ydl_opts = { 
-    'quiet': True, # don't throw status messages in the console
-    # 'outtmpl': downloadPath + r'/%(title)s.%(ext)s' # TODO: uncomment when line #55 changed 
-    } 
+# - check to not duplicate downloads - #
 
 try:
-    with yt_dlp.YoutubeDL(ydl_opts) as ydl:
-        info_dict = ydl.extract_info(videoURL, download=False) # get info about the video without downloading
-        # print("Getting video's title...") # status 
-        # video_title = info_dict.get('title', None) # get video's title
-        # print(f"Video's title: {video_title}") # print video's title
-        print("Downloading the video...") # status
-        ydl.download(videoURL) # now download the video
-        print("Download complete.") # status
-except: # Internet down, wrong URL
-    print("Can't download the video. Check your internet connection and video's URL, then try again. Closing...") # status
+    with open('videos.txt', 'r') as openFile: # open file
+        file_content = openFile.read() # read file
+except: 
+    with open('videos.txt', 'w') as openFile: # create a file
+        print("There was a problem with reading the `videos.txt` file. It didn't exist so it was created now.")
+    with open('videos.txt', 'r') as openFile: # open file
+        file_content = openFile.read() # read file
+    
+counter = 1 # NOTE: how far back we should look for the videos (3 = 4 videos back, 10 = 11 videos back, 0 = just newest video)
+# FIX: not working when eg. 2nd video downloaded and you're looking for the 1st and 3rd
+try:
+    if read_RSS.entries[counter].link in file_content:
+        print(f"Newest video already watched or downloaded and available on the disk.")
+    else:
+        while read_RSS.entries[counter].link not in file_content: # if video is new (not in the file of past downloads) then go and download; if not -> skip 
 
-# TODO: convert to .mp3?
+        # break if end of the list 
+            if counter < 0: 
+                break
+
+            print(f"Video # (1 = newest): {counter+1}") # debug / status
+
+            # channel URL
+            # print(f"Channel URL: {read_RSS.feed.link}") # print channel URL 
+
+            # video title
+            videoTitle = read_RSS.entries[counter].title # get latest video's title
+            print(f"Video's title: {videoTitle}") # print the latest video's URL
+
+            # video URL
+            videoURL = read_RSS.entries[counter].link # get the latest video's URL
+            print(f"Video's URL: {videoURL}") # print the latest video's URL
+
+            # download video
+            downloadVideo(videoURL)
+
+            counter = counter - 1 # go up the list to the [0] newest video 
+
+except: # file doesn't exist and can't read it, no internet connection, wrong feed URL
+    print("Issues with the checker. Closing...")
 
 # ----------- fun ends here ---------- #
 
