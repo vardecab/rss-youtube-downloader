b2ca88d8e0de129ff7dc82f19ff49e5bd1f36c02
vardecab <6877391+vardecab@users.noreply.github.com>
2022-09-18 12:33:23 +0200

    0.6.1

--- run.py
+++ run.py
@@ -3,7 +3,6 @@
 # ==================================== #
 
 # ------------ import libs ----------- #
-#region
 
 # core ↓
 import yt_dlp # download YouTube videos # NOTE: https://github.com/ytdl-org/youtube-dl/issues/30102#issuecomment-943849906
@@ -11,8 +10,6 @@ import feedparser # read RSS
 
 # other ↓
 import time # calculate script's run time
-# from datetime import datetime # generate timestamp for saving data
-# import os # create folders
 
 # notifications ↓ 
 from sys import platform # check platform (Windows/macOS)
@@ -22,20 +19,17 @@ elif platform == 'win32':
     from win10toast_click import ToastNotifier # Windows 10/11 notifications
     toaster = ToastNotifier() # initialize win10toast
     
-iconDownload = "icons/download.png"
-iconCheckmark = "icons/checkmark.png"
+iconDownload = "icons/download.png" # location of icon
+iconCheckmark = "icons/checkmark.png" # location of icon
 
-# import webbrowser # open URLs from notification
-
+# solution ↓
 # NOTE: fix certificate issue -> https://stackoverflow.com/questions/28282797/feedparser-parse-ssl-certificate-verify-failed
 if platform == 'darwin': # check if user is using macOS
     import ssl
     if hasattr(ssl, '_create_unverified_context'):
         ssl._create_default_https_context = ssl._create_unverified_context
 
-#endregion
 # --------- start + run time --------- #
-#region
 
 startTime = time.time() # run time start
 print("Starting the script...") # status
@@ -44,12 +38,12 @@ print("Starting the script...") # status
 
 # ---------- download video ---------- #
 
-# TODO: exclude segments with SponsorBlock parameters
-# TODO: convert to .mp3?
+# TODO: exclude segments with SponsorBlock parameters? might not work if download is triggered right after video is published
+# TODO: convert to .mp3? useful for music
 
 def downloadVideo(videoURL):
     
-    downloadPath = r'/Users/x/TV Shows/YouTube' # download location
+    downloadPath = r'/Users/x/TV Shows/YouTube Downloads' # download location
 
     #  parameters for the downloader
     optionalParameters = { 
@@ -63,43 +57,36 @@ def downloadVideo(videoURL):
             
             print("Downloading the video...") # status
             YouTubeDownloader.download(videoURL) # now download the video
-            # print("Video downloaded or it's already on the disk.") # status
             print("Video downloaded.") # status
             # TODO: how to check if downloading or already on disk?
             
             # notifications 
             if platform == "darwin": # macOS
                 pync.notify(
-                    # f"Video from {readRSS.feed.title} downloaded or already on the disk.",
                     f"Video from {readRSS.feed.title} downloaded.",
                     title='rss-youtube-downloader',
                     contentImage=iconDownload,
                     sound=""
-                    # open= # TODO: URL
+                    # open=videoURL
                     )
             elif platform == "win32": # Windows
                 toaster.show_toast(
                     title="rss-youtube-downloader", 
-                    msg=
-                    # f"Video from {readRSS.feed.title} downloaded or already on the disk.",
-                    f"Video from {readRSS.feed.title} downloaded.",
+                    msg=f"Video from {readRSS.feed.title} downloaded.",
                     icon_path="icons/download.ico",
                     duration=None,
                     threaded=True
-                    # callback_on_click= # TODO: check if works without it; function with URL
+                    # callback_on_click= 
                     ) # duration=None - leave notification in Notification Center; threaded=True - rest of the script will be allowed to be executed while the notification is still active
             
+            # save video URL to a file for checks in future
             with open('videos.txt','a') as saveFile: # create a text file
+                # duplicates are avoided because only new videos (not present in the file) rare saved
                 saveFile.write(videoURL + "\n") # save video URL to the file
-                # NOTE: not very pretty but works # TODO: avoid duplicates 
-
     except: # Internet down, wrong URL
         print(f"Can't download the video. Check your internet connection and video's URL ({videoURL}), then try again. Closing...") # status
 
 # ------------- read RSS ------------- #
-
-# TODO: get input + default on timeout 
-# TODO: ask which feed to check, type 1/2/3/4/5 or name of the channel 
 
 def readingRSS(RSSfeed):
     try:
@@ -107,8 +94,8 @@ def readingRSS(RSSfeed):
         print(f"Reading RSS feed: {readRSS.feed.title} ({RSSfeed})") # status
         return readRSS # get it outside of the function
     except: 
-        print(f"Can't read the RSS feed. Maybe there is a problem with your internet connection or with the `RSSfeed` URL: {RSSfeed}. Closing...")
-        exit()
+        print(f"Can't read the RSS feed. Maybe there is a problem with your internet connection or with the `RSSfeed` URL: {RSSfeed}. Closing...") # status
+        exit() # end the script
 
 # -------- add feeds to a list ------- #
 
@@ -122,12 +109,12 @@ print(f"How many feeds / channels we need to check: {len(RSSfeeds)}") # status
 
 # -- read / create .txt file w/ URLs - #
 
-try:
+try: # try to open file
     with open('videos.txt', 'r') as openFile: # open file
         fileContent = openFile.read() # read file
-except: 
+except: # if it doesn't exist, create it
     with open('videos.txt', 'w') as openFile: # create a file
-        print("There was a problem with reading the `videos.txt` file. It didn't exist so it was created now.")
+        print("There was a problem with reading the `videos.txt` file. It didn't exist so it was created now.") # status
     with open('videos.txt', 'r') as openFile: # open file
         fileContent = openFile.read() # read file
 
@@ -140,7 +127,7 @@ try:
         counterVideoLookback = 0 # NOTE: how far back we should look for the videos (3 = 4 videos back, 10 = 11 videos back, 0 = just newest video)
         # FIX: not working when eg. 2nd video downloaded and you're looking for the 1st and 3rd
         readRSS = readingRSS(RSSfeeds[counterFeedList]) #  throw feed to the function and get the data back so we can use it
-        if readRSS.entries[0].link in fileContent: # check if the latest video is watched / downloaded
+        if readRSS.entries[0].link in fileContent: # check if the newest video is watched / downloaded
             print(f"Newest video from {readRSS.feed.title} already watched or downloaded and available on the disk.")
             # notifications 
             if platform == "darwin": # macOS
@@ -149,26 +136,26 @@ try:
                     title='rss-youtube-downloader',
                     contentImage=iconCheckmark,
                     sound=""
-                    # open= # TODO: URL
+                    # open= 
                     )
             elif platform == "win32": # Windows
                 toaster.show_toast(
                     title="rss-youtube-downloader", 
-                    msg=
-                    f"No new videos from: {readRSS.feed.title}.",
+                    msg=f"No new videos from: {readRSS.feed.title}.",
                     icon_path="icons/checkmark.ico",
                     duration=None,
                     threaded=True
-                    # callback_on_click= # TODO: check if works without it; function with URL
+                    # callback_on_click=
                     ) # duration=None - leave notification in Notification Center; threaded=True - rest of the script will be allowed to be executed while the notification is still active
         else:
             while readRSS.entries[counterVideoLookback].link not in fileContent: # if video is new (not in the file of past downloads) then go and download; if not -> skip 
-            # break if end of the list 
+                
+            # break if we have enough videos 
                 if counterVideoLookback >= 2: # NOTE: how far we should look (2 = 3 videos); stop so we don't download too many videos 
-                    print(f"That should be enough. Moving on...") # status # TODO
+                    print(f"That should be enough. Moving on...") # status 
                     break
 
-                print(f"Downloading video # (1 = newest): {counterVideoLookback+1}") # debug / status
+                print(f"Downloading video # (1 = newest): {counterVideoLookback+1}") # status
 
                 # channel URL
                 # print(f"Channel URL: {readRSS.feed.link}") # print channel URL 
@@ -187,21 +174,17 @@ try:
                 # download video
                 downloadVideo(videoURL) # send URL to function
 
-                counterVideoLookback = counterVideoLookback + 1 # go up the list to the [0] newest video
+                counterVideoLookback = counterVideoLookback + 1 # go up the list to the older video
             else:
-                counterVideoLookback = counterVideoLookback + 1
-                # break
-        counterFeedList = counterFeedList + 1 # iterate through the feed list   
+                counterVideoLookback = counterVideoLookback + 1 # go up the list to the older video
+        counterFeedList = counterFeedList + 1 # continue with the next feed in the list   
 except: # file doesn't exist and can't read it, no internet connection, wrong feed URL
-    print("Issues with the checker. Closing...") # TODO: write something more descriptive
+    print("Issues with the checker. Closing...") # TODO: write something more descriptive and helpful
 
 # ----------- fun ends here ---------- #
 
 # ------------- run time ------------- #
-#region
 
 endTime = time.time() # run time end 
 totalRunTime = round(endTime-startTime,2)
 print(f"Total script run time: {totalRunTime} seconds. That's {round(totalRunTime/60,2)} minutes.")
-
-#endregion
